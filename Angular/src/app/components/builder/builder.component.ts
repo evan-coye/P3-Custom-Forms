8f42d4423f57fe88c1aee7c1c8c12a1c7bd67082
Katie Lewis <katielaurielewis@gmail.com>
2021-11-07 16:19:42 -0500

    added favicon, made some notes on custom components

--- Angular/src/app/components/builder/builder.component.ts
+++ Angular/src/app/components/builder/builder.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, ViewChild, EventEmitter, OnInit } from '@angular/core';
+import { Formio } from 'formiojs';
+
 @Component({
   selector: 'app-builder',
   templateUrl: './builder.component.html',
@@ -8,11 +10,48 @@ import { Component, ElementRef, ViewChild, EventEmitter, OnInit } from '@angular
 export class BuilderComponent implements OnInit {
 
   public form: Object = {
-  components: [],
+    components: []
   };
+
+  // according to: https://github.com/formio/angular/issues/528
+  //               https://github.com/formio/angular/issues/241
+  // this should be able to be passed into the form-builder element
+  // e.g. [options]="options"
+  // but the options input expects a different type than what is shown in the examples
+  options = {
+    builder: {
+      basic: false,
+      advanced: false,
+      data: false,
+      premium: false,
+      customBasic: {
+        title: 'My Basics',
+        default: true,
+        weight: 0,
+        components: {
+          textfield: true,
+          textarea: true
+        },
+      },
+      custom: {
+        title: 'Big Checks',
+        default: true,
+        weight: 10,
+        components: {
+          multicheckbuttons: true,
+          checkboxbutton: true,
+          radiobuttons: true,
+          yesnobuttons: true
+        }
+      }
+    }
+  };
+
   ngOnInit() {
+
   }
+
   save() {
-  console.log(this.form);
+    console.log(this.form);
   }
 }
